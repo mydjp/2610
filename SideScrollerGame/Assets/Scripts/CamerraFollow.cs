1651ab8015f512731b094b1d23f1034de9b2d972
Unknown <mydjp0912@yahoo.com>
2019-02-18 20:55:19 -0700

    finisshed sprint 1

---
+++ SideScrollerGame/Assets/Scripts/CamerraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerraFollow : MonoBehaviour
+{
+    public Vector3 camPosition;
+    public 
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SideScrollerGame/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float MoveSpeed = 5;
+    public float Gravity = -9.81f;
+
+    private CharacterController CC;
+    private Vector3 position;
+
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        position.z = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+        if (CC.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                position.y += 100;
+            }
+        }
+
+        position.y = Gravity * Time.deltaTime;
+        CC.Move(position);
+    }
+}
