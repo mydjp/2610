8f2e2db83f7e2534b8cd63935df2d8ab62ccc8ae
daniel <mydjp0912@yahoo.com>
2019-03-07 15:47:13 -0700

    got the Y follow to work

--- SideScrollerGame/Assets/Scripts/Lerp.cs
+++ SideScrollerGame/Assets/Scripts/Lerp.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Lerp : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject Player;
+    public GameObject Kraken;
+    public float YPosition;
+    public float TTime;
+
+
+    void Start()
+    {
+        YPosition = transform.position.y;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        YPosition = Mathf.Lerp(YPosition, Player.transform.position.y, TTime * Time.deltaTime);
+        Kraken.transform.position = new Vector3(Kraken.transform.position.x, YPosition, Kraken.transform.position.z);
     }
 }
