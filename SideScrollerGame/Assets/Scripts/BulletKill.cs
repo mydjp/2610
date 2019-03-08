140f48ee3d05bfbae42befcc268422e76619bc72
Daniel <mydjp0912@yahoo.com>
2019-03-08 14:30:05 -0700

    trying to make cannons shoot me

---
+++ SideScrollerGame/Assets/Scripts/BulletKill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletKill : MonoBehaviour
+{
+   public void OnTriggerEnter(Collider obj)
+   {
+      if(obj.gameObject.CompareTag("Bullet"))
+      {
+         Destroy(gameObject);
+      }
+   }
+}
